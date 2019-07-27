2bf67d0c11384ff8d72dc1324f3c29f72fb500cc
housewifehacker <housewifehacker@gmail.com>
2019-07-27 16:44:16 -0500

    build: refactor enzyme adaptor setup and getting mocked redux store

--- web/src/components/pages/login.test.js
+++ web/src/components/pages/login.test.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
-import Adapter from "enzyme-adapter-react-16";
-import configureStore from 'redux-mock-store';
+import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 
 import ConnectedLogin from '../../containers/login';
-
-configure({adapter: new Adapter()});
+import { getStore } from '../../tests/testUtils';
 
 test('Login button is disabled without input values', () => {
     const initialState = {
@@ -18,8 +15,7 @@ test('Login button is disabled without input values', () => {
             loginDisabled: true,
         }
     };
-    const mockStore = configureStore();
-    let store = mockStore(initialState);
+    let store = getStore(initialState);
     let component = mount(<Provider store={store}><ConnectedLogin /></Provider>);
 
     expect(component).toMatchSnapshot();
